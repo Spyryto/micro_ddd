93b4b014c45b5365414f5198fd429dac01069333
Spyryto <daniele.zecchin@gmail.com>
2021-09-24 18:00:42 +0200

    fix: Make result fields private

--- lib/src/result.dart
+++ lib/src/result.dart
@@ -2,26 +2,26 @@ enum ResultType { success, failure }
 
 class Result<S, F> {
   final ResultType _type;
-  Map<ResultType, dynamic> values = {};
+  final Map<ResultType, dynamic> _values = {};
 
   Result.success(S value) : _type = ResultType.success {
-    values[_type] = value;
+    _values[_type] = value;
   }
 
   Result.failure(F value) : _type = ResultType.failure {
-    values[_type] = value;
+    _values[_type] = value;
   }
 
   bool get isSuccess => _type == ResultType.success;
   bool get isFailure => _type == ResultType.failure;
 
   void match({required Function(S) success, required Function(F) failure}) {
-    var value = values[_type];
+    var value = _values[_type];
     isSuccess ? success(value) : failure(value);
   }
 
   T map<T>({required T Function(S) success, required T Function(F) failure}) {
-    var value = values[_type];
+    var value = _values[_type];
     return isSuccess ? success(value) : failure(value);
   }
 }
